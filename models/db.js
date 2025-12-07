61b7a6e0d47007640d01633a0230feab32d5dec6
Daniel Aimé Valcour <newjourneyman@yahoo.com>
2025-12-07 18:41:27 -0500

    Use openssl to connect to database if in production
    This hopefully fixes "internal server error" message

--- models/db.js
+++ models/db.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 
 // connection pool for clients to connect to database
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
