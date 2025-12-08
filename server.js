f310e4682700fb015d5e5923077ad33fb796fba0
Daniel Aimé Valcour <newjourneyman@yahoo.com>
2025-12-07 20:12:32 -0500

    Add small callback to log all incoming requests in server
    This will help with debugging. i have a lot of issues i am trying to fix

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// log all incoming requests
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 app.set('trust proxy', 1);
 
 app.use(session({
